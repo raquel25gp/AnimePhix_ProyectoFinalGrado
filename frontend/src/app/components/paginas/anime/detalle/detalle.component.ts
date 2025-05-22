f6f5278e3eec6cdafb093c999628b614a605709b
raquel25gp <raquel_gp27@hotmail.com>
2025-05-22 18:06:28 +0200

    Actualización detalle.component.ts
    Componente standalone

--- frontend/src/app/components/paginas/anime/detalle/detalle.component.ts
+++ frontend/src/app/components/paginas/anime/detalle/detalle.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-detalle',
+  standalone: true,
   imports: [],
   templateUrl: './detalle.component.html',
   styleUrl: './detalle.component.css'
