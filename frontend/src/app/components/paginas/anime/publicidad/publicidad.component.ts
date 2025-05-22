16a4e68d15e417bab312ef6e16dc5bdfabafc1c8
raquel25gp <raquel_gp27@hotmail.com>
2025-05-22 18:07:01 +0200

    Actualización publicidad.component.ts
    Componente standalone

--- frontend/src/app/components/paginas/anime/publicidad/publicidad.component.ts
+++ frontend/src/app/components/paginas/anime/publicidad/publicidad.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-publicidad',
+  standalone: true,
   imports: [],
   templateUrl: './publicidad.component.html',
   styleUrl: './publicidad.component.css'
